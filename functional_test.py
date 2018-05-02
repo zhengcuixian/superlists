1598401faf52991f142aa4a1ea298122c7c6856c
zhengcuixian <462845610@qq.com>
2018-05-02 22:36:55 +0800

    视图渲染模板板

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys 
 
 class NewVisitorTest(unittest.TestCase):
 	
@@ -29,7 +30,8 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1:Buy peacock feathers' for row in rows)
+			any(row.text == '1:Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table"
 		)
 		
 		self.fail('finish the test!')
