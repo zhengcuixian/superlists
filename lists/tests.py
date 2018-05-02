c5983e31fa71b01985ffcb6964c16c0ee5b7e864
zhengcuixian <462845610@qq.com>
2018-05-02 22:27:39 +0800

    使用模板修改主页视图

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
 #class SmokeTest(TestCase):
@@ -19,5 +20,10 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 		self.assertTrue(response.content.startswith(b'<html>'))
 		self.assertIn(b'<title>To-Do lists</title>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
+		self.assertTrue(response.content.strip().endswith(b'</html>'))
 		
+	def test_home_page_returns_correct_html2(self):
+		request = HttpRequest()
+		response = home_page(request)
+		expected_html = render_to_string('home.html')
+		self.assertEqual(response.content.decode(), expected_html)
--- lists/views.py
+++ lists/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 #home_page = None
 def home_page(request):
-	return HttpResponse('<html><title>To-Do lists</title></html>')
+	#return HttpResponse('<html><title>To-Do lists</title></html>')
+	return render(request, 'home.html')
+	
