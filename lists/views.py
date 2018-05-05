208e43d24007a95ff456ab59cb6608bc19da6b78
zhengcuixian <462845610@qq.com>
2018-05-05 22:55:03 +0800

    把post请求中的数据存入数据库

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from lists.models import Item 
 
 # Create your views here.
 #home_page = None
@@ -7,7 +8,11 @@ def home_page(request):
 	#return HttpResponse('<html><title>To-Do lists</title></html>')
 	#if request.method == 'POST':
 	#	return HttpResponse(request.POST['item_text'])
+	item = Item()
+	item.text = request.POST.get('item_text', '')
+	item.save()
+	
 	return render(request, 'home.html',
-		{'new_item_text': request.POST.get('item_text', ''),
+		{'new_item_text': item.text,
 	})
 	
